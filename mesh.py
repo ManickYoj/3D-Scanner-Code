442b650bad13aa71ee575d9b8795f2ac0d58a56a
unknown <nickfrancisci@gmail.com>
2014-04-19 11:46:52 -0400

    Completed, expanded, and tested the mesh class.

--- mesh.py
+++ mesh.py
@@ -2,13 +2,15 @@
 mesh.py
 -------
 Author: Nick Francisci
-Status: Incomplete & Untested
+Status: Complete & Tested
 Description: 
 A storage unit for meshs both as they are scanned and after a scan is complete.
 The mesh class also supports export methods.
+
 """
 
 import csv
+import os
 
 class Mesh(object):
     """
@@ -16,9 +18,10 @@ class Mesh(object):
     export methods.
     """
 
-    def __init__(self, name=None):
+    def __init__(self, name = None, savefolder = "Exported Meshes"):
         self.mesh = []
         self.name = name
+        self.savefolder = savefolder
     
     # ----- General Public Methods ----- #
 
@@ -44,17 +47,70 @@ class Mesh(object):
         """
         return self.name
 
+    def setsavefolder(self, savefolder):
+        """ 
+        Setter method for the field that determines the name of the directory in
+        which this mesh is saved.
+        """
+
+        self.savefolder = savefolder
+
     # ----- Public Export Methods ----- #
 
-    def exportcsv(self, filename):
+    def exportcsv(self, filename = None):
         """ Exports a file in excel CSV format. """
+        
+        filename = self.assignfilename(filename)
         with open(filename + '.csv', 'wb') as expfile:
             csvwriter = csv.writer(expfile)
             csvwriter.writerows(self.mesh)
 
+    # ----- Private Export Helper Methods ----- #
+
+    def assignfilename(self, filename):
+        """
+        A helper method for export methods that applies an appropriate
+        filename if none is specified.
+        """
+
+        if filename is None:
+            if self.name is not None:
+                filename = self.name
+            else:
+                filename = "default"
+
+        return self.assignfilepath(filename)
+
+
+    def assignfilepath(self, filename):
+        """
+        A helper method for export methods that appends an appropriate
+        file directory to the filename and creates that directory if it
+        does not exist.
+        """
+
+        if not os.path.exists(self.savefolder):
+            os.makedirs(self.savefolder)
+
+        return os.path.join(self.savefolder, filename)
+
+
+# ----- Unit Testing ----- #
+
+def runtests():
+    """ Runs methods of the Mesh class for testing. """
+    testmesh = Mesh("Test File")
+    testmesh.setname("Different Test File")
+    testmesh.setsavefolder("Test Exports")
+
+    testmesh.addpoint((15, 23, 34))
+    testmesh.addpoints([(134, 23, 34), (32, 17, 35)])
+    testmesh.exportcsv()
+
+    print testmesh.getname()
+    print testmesh.mesh
 
 if __name__ == "__main__":
-    TestMesh = Mesh()
-    TestMesh.addpoint((15,23,34))
-    TestMesh.addpoints([(134,23,34),(32,17,35)])
-    print TestMesh.mesh
+    runtests()
+
+
