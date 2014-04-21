a08d5b86630303f0a37ba8fc2de9a93410c23e58
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-04-21 11:42:32 -0400

    hardware methods are slightly better defined, but still need a lot of work to determine the state of rotation, which will hopefully be able to be done with new arduino interface class

--- hardware.py
+++ hardware.py
@@ -9,34 +9,43 @@ import cv2.cv
 import cv2
 import numpy as np
 import time
-import pyserial
+import serial
 
 class Hardware(object):
     def __init__(self, step_size = 1, camera = 1):
-        self.rotation = 0
-        self.step_size = step_size;
-        self.locked = False;
+        self.locked = False
         self.camera = camera
-        self.start_time = time.time()
+        self.start_time = -1
         self.frames = []
         self.done = False
-
+        self.end_time = -1
+        self.angle_vel = -1
+        self.rotation = False 
+        
+        
     def islocked(self):
         return self.locked;
 
     def togglelock(self):
         self.locked = not self.locked;
-
-    def getrotation(self):
-        return self.rotation;
-        
+      
     def isdone(self):
-        '''Checks once clears videocap, checks to see if rotated around
-        twice changes self.done to True'''
-        pass
+        '''Checks to see if rotated around'''
+        return self.done
         
-    def arduinocontrol(self):
-        pass
+    def checkrotation(self):
+        ser = serial.Serial('/dev/tty.usbserial', 9600)
+        
+    
+        
+    def beginscan(self):
+        '''starts the arduino and after one rotation, starts the camera'''
+        #insert arduino code
+        time.sleep(0.1)
+        if self.rotation:
+            self.videocap()
+        
+            
 
     def videocap(self):
         '''If not done, will take video and append each frame to a list 
@@ -46,6 +55,7 @@ class Hardware(object):
             # Take each frame
             _, frame = cap.read()
             self.frames.append((frame, time.time()-self.start_time))
+            time.sleep(0.1)
             
     def captureimage(self):
         '''If  scanning is not done, return the last image added to the list
@@ -54,9 +64,10 @@ class Hardware(object):
             return self.frames(-1)
         return None
                 
-    def conversionfactor(self, end_time):
-        factor = (end_time-self.start_time)/ (2*np.pi)
-        return factor            
+    def getavgvel(self):
+        if self.done:        
+            angle_vel= (self.end_time-self.start_time)/ (2*np.pi)
+            return angle_vel
         
         
         
