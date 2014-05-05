6915fc344fdc22bf122a7f56b82301b019268443
cta <celine.ta@students.olin.edu>
2014-05-04 23:44:40 -0400

    more minor changes; switch from csvreader to genfromtxt

--- testWirePlot.py
+++ testWirePlot.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-ax1.plot(data['x'], data['y'], color='r', label='the data')
 with open('box6.csv', 'rb') as csvfile:
-     points = csv.reader(csvfile, delimiter=' ', quotechar='|')
-     data = np.genfromtxt('e:\dir1\datafile.csv', delimiter=',', skip_header=10,skip_footer=10, names=['x', 'y', 'z'])
+     #points = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     data = np.genfromtxt('box6.csv', delimiter=',', skip_header=10,skip_footer=10, names=['x', 'y', 'z'])
      fig=plt.figure()
      ax = fig.add_subplot(111, projection='3d')
      ax.plot_wireframe(ax, points[0], points[1], points[2])
