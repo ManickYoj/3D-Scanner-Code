9713a68afdf746c8bf7dac5239ef71c0998844dc
cta <celine.ta@students.olin.edu>
2014-05-04 23:42:29 -0400

    more minor changes to testWirePlot

--- testWirePlot.py
+++ testWirePlot.py
@@ -1,11 +1,13 @@
 import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
+ax1.plot(data['x'], data['y'], color='r', label='the data')
 with open('box6.csv', 'rb') as csvfile:
      points = csv.reader(csvfile, delimiter=' ', quotechar='|')
-     print(type(points))
+     data = np.genfromtxt('e:\dir1\datafile.csv', delimiter=',', skip_header=10,skip_footer=10, names=['x', 'y', 'z'])
      fig=plt.figure()
      ax = fig.add_subplot(111, projection='3d')
-     Axes3D.plot_wireframe(ax, points[0], points[1], points[2])
+     ax.plot_wireframe(ax, points[0], points[1], points[2])
  # C:\Users\cta\Documents\GitHub\3D-Scanner-Code\Exported Meshes\box6
